b5636ccac250e1e751f08ce1341f3410b3765b05
audisiofilho <obuzua46@gmail.com>
2021-06-29 11:25:43 -0300

    começando projeto e configurando api

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+export default function App() {
+ return (
+   <div>
+     <BrowserRouter>
+        <Header/>
+        <Routes/>
+     </BrowserRouter>
+   </div>
+ );
+}
