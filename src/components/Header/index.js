66e648c1b75e2d167fc75e69e266df849f655c37
audisiofilho <obuzua46@gmail.com>
2021-06-29 15:08:37 -0300

    solicitando e adicionando reserva

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './style.css';
 
 import logo from '../../assets/logo.svg';
 
 export default function Header() {
+  const reserveSize = useSelector(state => state.reserve)
+
  return (
    <header className="container">
        <Link to='/'>
@@ -14,7 +17,7 @@ export default function Header() {
        <Link className="reserva" to="/reservas">
         <div>
             <strong>Minhas Reservas</strong>
-            <span>0 reservas</span>
+            <span>{reserveSize.length} reservas</span>
         </div>
        </Link>
    </header>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
 import api from "../../services/api";
 
 import { MdFlightTakeoff } from 'react-icons/md'
 import './style.css';
 
 export default function Home() {
+  const dispatch = useDispatch();
   const [trips, setTrips] = useState([]);
 
   useEffect(() => {
@@ -15,6 +17,14 @@ export default function Home() {
 
     loadApi();
   }, []);
+
+  function handleAdd(trip){
+    dispatch({
+      type: "ADD_RESERVE",
+      trip
+    })
+  }
+
   return (
     <div>
       <div className="box">
@@ -24,7 +34,7 @@ export default function Home() {
             <strong>{trip.title}</strong>
             <span>Status: {trip.status ? "Disponivel" : "Indisponivel"}</span>
 
-            <button type="button" onClick={()=>{}}>
+            <button type="button" onClick={()=> handleAdd(trip)}>
               <div>
                 <MdFlightTakeoff size={16} color="#fff" />
               </div>
