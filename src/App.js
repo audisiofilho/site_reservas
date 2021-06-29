6917074a6861ccaa6303057adf4fdf6ac1cc808c
audisiofilho <obuzua46@gmail.com>
2021-06-29 14:35:20 -0300

    Adicionando redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 import Routes from './routes';
 import Header from './components/Header';
 
 export default function App() {
  return (
-   <div>
+   <Provider store={store}>
      <BrowserRouter>
         <Header/>
         <Routes/>
      </BrowserRouter>
-   </div>
+   </Provider>
  );
 }
