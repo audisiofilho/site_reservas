9d9c80de2d0d65871e48dfda3e8513723ba05ada
audisiofilho <obuzua46@gmail.com>
2021-07-06 15:14:14 -0300

    Refatorando com Redux-Saga

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,14 +1,29 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
-import { addReverveSuccess } from './actions';
+import { all, call, put, takeLatest, select } from "redux-saga/effects";
+import { addReverveSuccess, updateAmountReserve } from "./actions";
 
-import api from "../../../services/api"
+import api from "../../../services/api";
 
-function* addToReserve({ id }){
+function* addToReserve({ id }) {
+  const tripExists = yield select((state) =>
+    state.reserve.find((trip) => trip.id === id)
+  );
+
+  if (tripExists) {
+    //Aumentar o amount
+    const amount = tripExists.amount + 1;
+
+    yield put(updateAmountReserve(id, amount))
+
+  } else {
     const response = yield call(api.get, `trips/${id}`);
 
-    yield put(addReverveSuccess(response.data));
+    const data = {
+        ...response.data,
+        amount: 1,
+    }
+
+    yield put(addReverveSuccess(data));
+  }
 }
 
-export default all([
-    takeLatest('ADD_RESERVE_REQUEST', addToReserve)
-]);
+export default all([takeLatest("ADD_RESERVE_REQUEST", addToReserve)]);
